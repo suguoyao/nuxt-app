4ebd7541c2784f0d5db799342bf203d191664a88
suguoyao <343166031@qq.com>
2018-12-26 21:58:57 +0800

    pages文件夹下新建.vue自动生成路由

---
+++ pages/sugar.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <div>测试新页面，pages文件夹下的.vue文件就是一个页面，nuxt会自动生成一个route</div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Sugar',
+    data() {
+      return {}
+    },
+    computed: {},
+    created() {
+
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
